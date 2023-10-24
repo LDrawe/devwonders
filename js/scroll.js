e1e17a51f81042ce32b2197ba7b734698ff99736
Eduardo Portilho <eduardomportilho@hotmail.com>
2023-10-24 18:27:21 -0300

    Feat: Scroll button auto show

--- js/scroll.js
+++ js/scroll.js
@@ -1,5 +1,12 @@
 const container = document.querySelector("article")
 
+container.onscroll = () => {
+    const returnButton = document.getElementById("return")
+    const shouldShow = container.scrollTop > 300
+    returnButton.classList.toggle("visible", shouldShow)
+    returnButton.classList.toggle("hidden", !shouldShow)
+}
+
 function resetScroll() {
     container.scrollTop = 0 // Set the scroll position to the top
 }
