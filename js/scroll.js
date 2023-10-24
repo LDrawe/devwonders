ed89ca60411668760015caae66cdaba2b0e76e1e
Eduardo Portilho <eduardomportilho@hotmail.com>
2023-10-23 23:40:49 -0300

    Feat: Return to top button

---
+++ js/scroll.js
@@ -0,0 +1,4 @@
+function resetScroll() {
+    var container = document.querySelector("section"); // Get a reference to the container
+    container.scrollTop = 0; // Set the scroll position to the top
+}
