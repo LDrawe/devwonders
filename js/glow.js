ded74d3812b3e8196d6f4a2cb482e86366890cbe
Eduardo Portilho <eduardomportilho@gmail.com>
2024-06-11 08:54:47 -0300

    Fix: Wrong syntax

--- js/glow.js
+++ js/glow.js
@@ -2,9 +2,9 @@ const cards = document.querySelectorAll('.card')
 
 function handleHover(e) {
     const { currentTarget: target } = e
-    const rect = target.getBoundingClientRect(),
-    x = e.clientX - rect.left
-    y = e.clientY - rect.top
+    const rect = target.getBoundingClientRect()
+    let x = e.clientX - rect.left
+    let y = e.clientY - rect.top
 
     target.style.setProperty('--mouse-x', `${x}px`)
     target.style.setProperty('--mouse-y', `${y}px`)
